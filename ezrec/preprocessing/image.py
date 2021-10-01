c19210c68e917b894cd46e918aaeb2bc7ca6c5b1
valentin-fngr <valentin.fontanger@gmail.com>
2021-10-01 21:42:14 +0200

    test create tf records

--- ezrec/preprocessing/image.py
+++ ezrec/preprocessing/image.py
@@ -3,9 +3,25 @@ import numpy as np
 import PIL
 import timeit
 import os
+import sys
 
 image_folder = os.path.join("/home/valentin/Desktop/deep_learning/ezrec/ezrec/images")
 image_file = os.path.join(image_folder,"0a1fe8530f26.jpg")
+
+
+def _bytes_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  if isinstance(value, type(tf.constant(0))):
+    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _float_feature(value):
+  """Returns a float_list from a float / double."""
+  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _int64_feature(value):
+  """Returns an int64_list from a bool / enum / int / uint."""
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
 
@@ -20,3 +36,35 @@ def load_image(image_path):
     img = np.asarray(PIL.Image.open(image_path), dtype=np.float32)
     return img
 
+
+
+def create_image_record(image_path): 
+    
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_png(img)
+    image_shape = img.shape
+    # flatten image to one D array
+    img = tf.reshape(img, -1).numpy()
+
+
+    feature = {
+        "image" : tf.train.Feature(float_list=tf.train.FloatList(value=img)), 
+        "shape" : tf.train.Feature(float_list=tf.train.FloatList(value=image_shape))
+    }
+
+    feature = tf.train.Features(feature=feature)
+
+    example = tf.train.Example(features=feature)
+    
+    return example.SerializeToString()
+
+
+
+def write_tf_records(records_list, file_name): 
+    print(f"{len(records_list)} files given")
+    with tf.io.TFRecordWriter(file_name) as writer:
+        for record in records_list: 
+            writer.write(record)
+
+    print("complete")
+    return 
--- ezrec/tests/load_image_test.py
+++ ezrec/tests/load_image_test.py
@@ -1,5 +1,36 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../ezrec")
-from  preprocessing.image import load_image, load_string_image
+from  preprocessing.image import load_image, load_string_image, create_image_record, write_tf_records
 import unittest
+import PIL
+import numpy as np
+
+
+image_folder = os.path.join("/home/valentin/Desktop/deep_learning/ezrec/ezrec/images")
+image_path = os.path.join(image_folder,"0a1fe8530f26.jpg")
+
+
+
+class TestRecordMethods(unittest.TestCase): 
+
+    def test_create_save_records(self): 
+        """
+            Create tf record, encode the image and its shape. 
+            Retrieve the data 
+
+            TODO : add more records
+        """
+        records_path = "test_records"
+
+        tf_record_example = create_image_record(image_path) 
+        records = [tf_record_example]
+        write_tf_records(records, "test_records")
+
+        # check if the file has been created
+        self.assertTrue(os.path.exists("test_records"))
+    
+        os.remove("test_records")
+
+if __name__ == '__main__':
+    unittest.main()
